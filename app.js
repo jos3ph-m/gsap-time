cf7de63213945ac81bc9016e7ffbda3d814f46d1
Joseph M <jmedina1985@hotmail.com>
2020-09-17 17:27:16 -0700

    Playing with stagger and delay

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-gsap.from(".content", {
-  opacity: 0.1,
+gsap.from(".anim1", {
+  opacity: 0,
   duration: 2,
-  y: -400,
+  y: -350,
   ease: "elastic(1, .9)",
+  stagger: 0.6,
+});
+
+gsap.from(".swirl", {
+  opacity: 0.1,
+  duration: 4,
+  y: 400,
+  ease: "elastic(1, .9)",
+  delay: 1.4,
+});
+
+gsap.from("aside", {
+  opacity: 0,
+  backgroundPosition: "200px 0px",
+  duration: 5,
+  ease: "elastic(1, .9)",
+  delay: 1.1,
 });
