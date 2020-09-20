d4d4e6426585aa2c752d46db33f7f4a2b54c9117
Joseph M <jmedina1985@hotmail.com>
2020-09-20 00:04:15 -0700

    Adjusted to timeline format

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-let rule = CSSRulePlugin.getRule("span:after");
+const rule = CSSRulePlugin.getRule("span:after");
 
+/*
 gsap.from(".anim1", {
   opacity: 0,
   duration: 2,
@@ -25,3 +26,41 @@ gsap.from("aside", {
   ease: "elastic(1, .9)",
   delay: 1.1,
 });
+*/
+const tl = gsap.timeline();
+
+tl.from(".anim1", {
+  duration: 2,
+  y: -350,
+  stagger: 0.6,
+  opacity: 0,
+  ease: "elastic(1, .9)",
+})
+  .to(
+    rule,
+    {
+      duration: 1.5,
+      cssRule: { scaleY: 0 },
+    },
+    "-=1.2"
+  )
+  .from(
+    "aside",
+    {
+      opacity: 0,
+      backgroundPosition: "200px 0px",
+      duration: 5,
+      ease: "elastic(1, .9)",
+    },
+    "-=1.2"
+  )
+  .from(
+    ".swirl",
+    {
+      opacity: 0,
+      duration: 4,
+      y: 300,
+      ease: "elastic(1, .9)",
+    },
+    "-=4"
+  );
